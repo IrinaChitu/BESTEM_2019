f8cb2b779dd37f29e06a77001bea74b62c2dc60a
TooHappy22 <teodormarchitan@gmail.com>
2019-04-06 17:26:31 +0300

    Speech to text added

---
+++ JS/authorizationToken.js
@@ -0,0 +1,16 @@
+function RequestAuthorizationToken() {
+    if (authorizationEndpoint) {
+        var a = new XMLHttpRequest();
+        a.open("GET", authorizationEndpoint);
+        a.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        a.send("");
+        a.onload = function () {
+            var token = JSON.parse(atob(this.responseText.split(".")[1]));
+            regionOptions.value = token.region;
+            authorizationToken = this.responseText;
+            key.disabled = true;
+            key.value = "using authorization token (hit F5 to refresh)";
+            console.log("Got an authorization token: " + token);
+        }
+    }
+}
