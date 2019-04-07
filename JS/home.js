daef1367ed62ecc53089482107f6aba621f0f33d
TooHappy22 <teodormarchitan@gmail.com>
2019-04-07 05:56:09 +0300

    createRoomInterface

--- JS/home.js
+++ JS/home.js
@@ -6,15 +6,59 @@ function loading() {
 }
 
 window.onload = function() {
+    loadProfiles();
+    let users = getProfiles();
+
+    let peopleSelected = document.getElementById("peopleSelectedTextArea");
 
     document.getElementById("readyToSpeak").onclick = function() {
         document.getElementById('timer').style.display='block';
         setTimeout(loading, 10000);
         // document.getElementById("formularInscriere").style.display = "none";
         enrollNewProfile();
-    }
+    };
 
+    document.getElementById("createRoomButton").addEventListener('click', function () {
+        document.getElementById('createRoom').style.display='block';
 
+        console.log("We have to add users now");
+        for (let i = 0; i < users.length; ++i) {
+            let newOption = document.createElement("OPTION");
+            newOption.innerText = users[i].getName();
+            document.getElementById("peopleSelection").appendChild(newOption);
+        }
+
+        let options = document.getElementById("peopleSelection").children;
+        let selectedOptions = [options.length];
+
+        for (let i = 0; i < options.length; ++i) {
+            selectedOptions[i] = 0;
+        }
+
+        for (let i = 0; i < options.length; ++i) {
+            options[i].addEventListener('click', function() {
+                selectedOptions[i]++;
+
+                if (selectedOptions[i] === 1) {
+                    console.log(options[i].innerHTML);
+                    // console.log(document.getElementById("peopleSelectedTextArea").innerText);
+                    // let text = document.getElementById("peopleSelectedTextArea").innerText + options[i].innerHTML + ", ";
+                    // console.log(text);
+                    // document.getElementById("peopleSelectedTextArea").innerText = text;
+
+                    let newP = document.createElement("P");
+                    newP.innerText = options[i].innerHTML;
+
+                    peopleSelected.appendChild(newP);
+                }
+
+                options[i].style.backgroundColor = "lightblue";
+            });
+        }
+
+        document.getElementById("nextButtonCreateRoom").addEventListener('click', function() {
+            location.replace("HTML/room.html");
+        });
+    });
 };
-
 
