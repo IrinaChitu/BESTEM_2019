3c1bcd573af20ec6eae745b59d3446820fe54f1e
TooHappy22 <teodormarchitan@gmail.com>
2019-04-07 08:14:26 +0300

    Database added

---
+++ JS/dbConnection.js
@@ -0,0 +1,18 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAuHMTUxKwIuLhCD6icuyAaap1Ic4CpzEU",
+    authDomain: "bestem19-b856d.firebaseapp.com",
+    databaseURL: "https://bestem19-b856d.firebaseio.com",
+    projectId: "bestem19-b856d",
+    storageBucket: "bestem19-b856d.appspot.com",
+    messagingSenderId: "804641905361"
+};
+
+firebase.initializeApp(config);
+var db = firebase.firestore();
+
+db.collection("Users").doc("profileID").set({
+    nume : "Ion",
+    profileID : "123-495"
+});
